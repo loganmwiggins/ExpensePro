235d8cc93368a98937ed89a2301dd52e555af912
lmw20c <lmw20c@fsu.edu>
2024-06-03 20:42:56 -0400

    routing bug fixes

--- src/app/all-expenses/all-expenses.component.ts
+++ src/app/all-expenses/all-expenses.component.ts
@@ -1,16 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { ExpenseTablesComponent } from '../expense-tables/expense-tables.component';
 
 @Component({
     selector: 'app-all-expenses',
     standalone: true,
-    imports: [ CommonModule, ExpenseTablesComponent ],
+    imports: [ CommonModule, RouterModule, ExpenseTablesComponent ],
     templateUrl: './all-expenses.component.html',
     styleUrl: './all-expenses.component.css'
 })
 
-export class AllExpensesComponent {
-
-}
+export class AllExpensesComponent {}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 import { AppComponent } from './app/app.component';
 import routeConfig from './routes';
 
 bootstrapApplication(AppComponent, {
     providers: [
+        provideHttpClient(withFetch()),
         provideRouter(routeConfig)
     ]
 }).catch((err) => console.error(err));
