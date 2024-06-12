43532e199dacb28dfe7e2c98805b97ce47fb2b51
lmw20c <lmw20c@fsu.edu>
2024-06-12 16:09:32 -0400

    add delete button to edit expense page

--- expenses-angular/src/app/components/edit-expense/edit-expense.component.ts
+++ expenses-angular/src/app/components/edit-expense/edit-expense.component.ts
@@ -193,4 +193,19 @@ export class EditExpenseComponent {
                 });
         }
     }
+
+    // [HttpDelete]
+    deleteExpense() {
+        if (window.confirm("Are you sure you want to delete this expense?")) {  //Confirmation request
+            this.http.delete(`https://localhost:7265/api/Expenses/${this.expenseId}`)
+              .subscribe({
+                  // Will only run when we get a success response from API
+                  next: (response) => {
+                    this.router.navigate(['/']); // Redirect to the dashboard
+                    this.toast.success("Expense deleted successfully.", "SUCCESS", 5000);
+                    this.ngOnInit();
+                  }
+              });
+        }
+    }
 }
